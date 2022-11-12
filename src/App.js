24b096ac370e7c4a526a6017bdd7075afeadfef1
franzavala <francizavala15@gmail.com>
2022-11-12 00:15:09 -0300

    changes in pagination

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Characters from "./components/Characters";
+import Pagination from "./components/Pagination";
+import Navbar from './components/Navbar'
 
-function App() {
+
+
+function AppRM() {
+  const [characters, setCharacters] = useState([]);
+  const [info, setInfo] = useState({})
+
+  const initialUrl = "https://rickandmortyapi.com/api/character"
+
+  const fecthCharacters = (url) => {
+    fetch(url)
+      .then(response => response.json())
+      .then((data) => {
+        setCharacters(data.results);
+        setInfo(data.info);
+      })
+
+      .catch((error) => console.log(error));
+  };
+
+  const onPrevious = () => {
+    fecthCharacters(info.prev)
+  }
+
+  const onNext = () => {
+    fecthCharacters(info.next)
+
+  }
+
+
+  useEffect(() => {
+    fecthCharacters(initialUrl);
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <Navbar />
+      <Pagination prev={info.prev} next={info.next} onPrevious={onPrevious} onNext={onNext} />
+      <div className="container mt-5 ">
+        <Characters characters={characters} />
+      </div>
+      <Pagination prev={info.prev} next={info.next} onPrevious={onPrevious} onNext={onNext} />
+    </>
+
+
   );
 }
 
-export default App;
+export default AppRM;
